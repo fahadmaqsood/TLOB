787acc15b5c18926f9bf96867f7c16903490140a
Fahad Maqsood Qazi <qazifahadmaqsood@gmail.com>
2025-11-27 14:51:33 +0000

    Adding support for databento format

--- preprocessing/databento_fi_2010.py
+++ preprocessing/databento_fi_2010.py
@@ -17,39 +17,56 @@ def databento_fi_2010_load(path, seq_size, horizon, all_features):
     # full_test = np.hstack((dec_test1, dec_test2, dec_test3))
 
     # Load dataset using pandas
-    df = pd.read_csv(path + "/databento_fi2010_2025-09-15_2025-09-15.txt", 
-                    sep="\t")
+    dec_data = np.loadtxt("data/DATABENTO/databento_fi2010_2025-09-15_2025-09-15.txt")
 
-    # Separate features and labels
-    data = df.iloc[:, :-1].values       # time × features
-    labels = df.iloc[:, -1].values.astype(int) - 1
+    full_train = dec_data[:, :int(dec_data.shape[1] * cst.SPLIT_RATES[0])]
+    full_val = dec_data[:, int(dec_data.shape[1] * cst.SPLIT_RATES[0]):]
+    full_test = full_val
 
-    # Stratified split: train 80%, val+test 20%
-    X_train, X_temp, y_train, y_temp = train_test_split(
-        data, labels, test_size=0.2, stratify=labels, random_state=42
-    )
+    horizon = 10
+    seq_size = 128
+    all_features = True
 
-    # Split remaining 20% into val and test (10% each)
-    X_val, X_test, y_val, y_test = train_test_split(
-        X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=42
-    )
+    if horizon == 10:
+        tmp = 5
+    elif horizon == 20:
+        tmp = 4
+    elif horizon == 30:
+        tmp = 3
+    elif horizon == 50:
+        tmp = 2
+    elif horizon == 100:
+        tmp = 1
+    else:
+        raise ValueError("Horizon not found")
 
-    # Transpose back to original FI-2010 style (features × time)
-    full_train = X_train
-    full_val   = X_val
-    full_test  = X_test
+    train_labels = full_train[-tmp, :].flatten()
+    val_labels = full_val[-tmp, :].flatten()
+    test_labels = full_test[-tmp, :].flatten()
 
-    train_input = torch.from_numpy(X_train).float()
-    train_labels = torch.from_numpy(y_train).long()
-    val_input = torch.from_numpy(full_val).float()
-    val_labels = torch.from_numpy(y_val).long()
-    test_input = torch.from_numpy(full_test).float()
-    test_labels = torch.from_numpy(y_test).long()
+    print(np.unique(train_labels))
+
+    train_labels = train_labels[seq_size-1:] - 1
+    val_labels = val_labels[seq_size-1:] - 1
+    test_labels = test_labels[seq_size-1:] - 1
+    if all_features:
+        train_input = full_train[:144, :].T
+        val_input = full_val[:144, :].T
+        test_input = full_test[:144, :].T
+    else:
+        train_input = full_train[:40, :].T
+        val_input = full_val[:40, :].T
+        test_input = full_test[:40, :].T
+    train_input = torch.from_numpy(train_input).float()
+    train_labels = torch.from_numpy(train_labels).long()
+    val_input = torch.from_numpy(val_input).float()
+    val_labels = torch.from_numpy(val_labels).long()
+    test_input = torch.from_numpy(test_input).float()
+    test_labels = torch.from_numpy(test_labels).long()
 
     print("Unique labels in train:", np.unique(train_labels))
     print("Unique labels in val:", np.unique(val_labels))
     print("Unique labels in test:", np.unique(test_labels))
 
-    return train_input, train_labels, val_input, val_labels, test_input, test_labels
-    
+    return train_input.shape, train_labels.shape, val_input.shape, val_labels.shape, test_input.shape, test_labels.shape
     
