724b55e41ad4305ac1bce6447761e956921ee78a
Fahad Maqsood Qazi <qazifahadmaqsood@gmail.com>
2025-12-01 17:30:58 +0000

    fixing lobster format

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ["TORCH_USE_CUDA_DSA"] = "1"
+
+import os
 import random
 import warnings
 
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ["TORCH_USE_CUDA_DSA"] = "1"
+
 import lightning as L
 import omegaconf
 import torch
